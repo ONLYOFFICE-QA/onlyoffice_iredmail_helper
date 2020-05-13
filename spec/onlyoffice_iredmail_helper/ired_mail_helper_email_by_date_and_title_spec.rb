0896ab38c23b4ec5fd83f4e1c46ebec683c9a56c
Pavel Lobashov <shockwavenn@gmail.com>
2020-05-13 15:46:12 +0300

    Add ability to move_out in email_by_date_and_title

--- spec/onlyoffice_iredmail_helper/ired_mail_helper_email_by_date_and_title_spec.rb
+++ spec/onlyoffice_iredmail_helper/ired_mail_helper_email_by_date_and_title_spec.rb
@@ -6,7 +6,10 @@ RSpec.describe OnlyofficeIredmailHelper::IredMailHelper, '#email_by_date_and_tit
   mail_helper = described_class.new
   let(:title) { "email title: #{Time.now.nsec}" }
 
-  before { mail_helper.delete_all_messages }
+  before do
+    mail_helper.delete_all_messages
+    mail_helper.send_mail(subject: title, mailto: mail_helper.username)
+  end
 
   it 'email_by_date_and_message' do
     mail_helper.send_mail(subject: title, mailto: mail_helper.username)
@@ -18,5 +21,22 @@ RSpec.describe OnlyofficeIredmailHelper::IredMailHelper, '#email_by_date_and_tit
     mail_helper.send_mail(subject: title, mailto: mail_helper.username)
     expect(mail_helper.email_by_date_and_title(subject: 'email title',
                                                timeout: 10)[:subject]).to eq(title)
+  end
+
+  describe 'move_out messages' do
+    it 'move_out message is enable by default' do
+      mail_helper.email_by_date_and_title(subject: title,
+                                          timeout: 10)
+      expect(mail_helper.email_by_date_and_title(subject: title,
+                                                 timeout: 10)).to be_falsey
+    end
+
+    it 'move_out can be disabled' do
+      mail_helper.email_by_date_and_title(subject: title,
+                                          timeout: 10,
+                                          move_out: false)
+      expect(mail_helper.email_by_date_and_title(subject: 'email title',
+                                                 timeout: 10)[:subject]).to eq(title)
+    end
   end
 end
