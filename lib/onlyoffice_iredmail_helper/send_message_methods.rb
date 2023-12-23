73507ff232b941b933aefb1fff2569bea3d445cb
Vitaliy Serov <vitality.serov@onlyoffice.com>
2023-12-23 11:45:59 +0200

    Extract creating SMTP connection to separate method

--- lib/onlyoffice_iredmail_helper/send_message_methods.rb
+++ lib/onlyoffice_iredmail_helper/send_message_methods.rb
@@ -13,7 +13,7 @@ module OnlyofficeIredmailHelper
         Subject: #{msg_data[:subject]}
         Date: #{Time.now.rfc2822}
         Message-Id: "#{Digest::SHA2.hexdigest(Time.now.to_i.to_s)}@#{@username.split('@').last}"
-        
+
         #{msg_data[:body]}
       END_OF_MESSAGE
     end
@@ -25,9 +25,19 @@ module OnlyofficeIredmailHelper
       options[:subject] ||= @default_subject
       options[:body] ||= @default_body
       options[:mailto] ||= @default_user
-      smtp = Net::SMTP.start(@domainname, 25, @username, @username, @password, :login)
+      smtp = create_smtp_connection
       smtp.send_message create_msg(options), @username, options[:mailto]
       smtp.finish
+    end
+
+    private
+
+    # Method that will create an SMTP connection to configure server
+    # @return [Net:SMTP] created connection
+    def create_smtp_connection
+      Net::SMTP.start(@domainname, 25, @username, @username, @password, :login)
+    rescue StandardError => e
+      raise e.class, "Can't create SMTP connection because of #{e}"
     end
   end
 end
